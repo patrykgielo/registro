55efaab72b20922253f1817c5be4003abd7bf0d1
Patryk Gielo <g.patryk@foozagency.com>
2026-04-01 00:24:14 +0200

    fix(alpine): install @alpinejs/focus plugin required by x-trap directive

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 import './booking-wizard';
 import Alpine from 'alpinejs';
+import Focus from '@alpinejs/focus';
 
+Alpine.plugin(Focus);
 window.Alpine = Alpine;
 Alpine.start();
 
